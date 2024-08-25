e2acec048d53f6c55b76e74ac17ff3ed3819fb39
reo0220 <k_reo0220@icloud.com>
2024-08-25 19:23:38 +0900

    profile.php修正

--- profile.php
+++ profile.php
@@ -59,7 +59,6 @@
 
             //usersテーブルとpostsテーブルとuser_mediasテーブルとpost_mediasテーブルを結合
             $sql_post = "SELECT
-                         DISTINCT 
                             posts.post_id,
                             posts.user_id,
                             posts.name AS posts_name,
